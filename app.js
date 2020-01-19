913b6a2c1d683d31d018003cb251f7f730d68975
mic-meier <mic.l.meier@gmail.com>
2020-01-19 15:43:38 +0100

    helper functions and unit tests - step1

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-const config = require("./utils/config");
+const config = require('./utils/config');
 
-const express = require("express");
+const express = require('express');
 const app = express();
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const blogsRouter = require("./controllers/blogs")
-const mongoose = require("mongoose");
-
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const blogsRouter = require('./controllers/blogs');
+const mongoose = require('mongoose');
 
 const mongoUrl = config.MONGODB_URI;
 mongoose.connect(mongoUrl, { useNewUrlParser: true });
@@ -14,6 +13,6 @@ mongoose.connect(mongoUrl, { useNewUrlParser: true });
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use("/api/blogs", blogsRouter)
+app.use('/api/blogs', blogsRouter);
 
 module.exports = app;
