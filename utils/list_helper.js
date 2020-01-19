22f58a91689cdc9aecfcf0e7d893104f9ebd02ae
mic-meier <mic.l.meier@gmail.com>
2020-01-19 16:03:52 +0100

    helper functions and unit tests - step2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,19 @@ const dummy = () => {
   return 1;
 };
 
+const totalLikes = blogs => {
+  if (blogs.length === 0) {
+    return 0;
+  }
+
+  const result = blogs
+    .map(blog => blog.likes)
+    .reduce((sum, likes) => sum + likes);
+
+  return result;
+};
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 };
